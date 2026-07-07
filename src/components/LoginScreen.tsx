221dfc029b3d77c4ebdd78bcda08cfb76712a672
emon11vk <emon11vk@gmail.com>
2026-07-07 23:03:20 +0700

    d

--- src/components/LoginScreen.tsx
+++ src/components/LoginScreen.tsx
@@ -35,7 +35,7 @@ export default function LoginScreen({ theme, onLoginSuccess, toggleTheme }: Logi
       const { error } = await supabase.auth.signInWithOAuth({
         provider: 'google',
         options: {
-          redirectTo: window.location.origin,
+          redirectTo: window.location.origin + '/',
         },
       });
 
